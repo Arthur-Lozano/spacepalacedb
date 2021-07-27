755ec92b6a0c23cf7f25e9d73fb1fcbd78c4b700
Arthur Lozano <chillout885@gmail.com>
2021-07-27 14:25:30 -0700

    completed contact form page/serverpage

--- src/config/config.js
+++ src/config/config.js
@@ -2,11 +2,13 @@ const nodemailer = require('nodemailer');
 const dotenv = require('dotenv');
 dotenv.config();
 
+console.log(process.env.EMAIL)
+console.log(process.env.PASSWORD)
 var transporter = nodemailer.createTransport({
   service: 'gmail',
   auth: {
-    user: process.env.email, // your email address to send email from
-    pass: process.env.password // your gmail account password
+    user: process.env.EMAIL, // your email address to send email from
+    pass: process.env.PASSWORD // your gmail account password
   }
 });
 
--- src/routes/email-route.js
+++ src/routes/email-route.js
@@ -5,17 +5,16 @@ const dotenv = require('dotenv');
 dotenv.config();
 const emailRouter = express.Router();
 
-const buildPath = path.join(__dirname, '..', 'build');
+// const buildPath = path.join(__dirname, '..', 'build');
 emailRouter.use(express.json());
-emailRouter.use(express.static(buildPath));
+// emailRouter.use(express.static());
 
 emailRouter.post('/send', (req, res) => {
-  try {
-    const mailOptions = {
-      from: req.body.email, // sender address
-      to: process.env.email, // list of receivers
-      subject: req.body.subject, // Subject line
-      html: `
+  const mailOptions = {
+    from: req.body.email, // sender address
+    to: process.env.EMAIL, // list of receivers
+    subject: req.body.subject, // Subject line
+    html: `
       <p>You have a new contact request.</p>
       <h3>Contact Details</h3>
       <ul>
@@ -25,27 +24,21 @@ emailRouter.post('/send', (req, res) => {
         <li>Message: ${req.body.message}</li>
       </ul>
       `
-    };
+  };
 
-    transporter.sendMail(mailOptions, function (err, info) {
-      if (err) {
-        res.status(500).send({
-          success: false,
-          message: 'Something went wrong. Try again later'
-        });
-      } else {
-        res.send({
-          success: true,
-          message: 'Thanks for contacting us. We will get back to you shortly'
-        });
-      }
-    });
-  } catch (error) {
-    res.status(500).send({
-      success: false,
-      message: 'Something went wrong. Try again later'
-    });
-  }
+  transporter.sendMail(mailOptions, function (err, info) {
+    if (err) {
+      res.status(500).send({
+        success: false,
+        message: 'Something went wrong. Try again later'
+      });
+    } else {
+      res.send({
+        success: true,
+        message: 'Thanks for contacting us. We will get back to you shortly'
+      });
+    }
+  });
 });
 
 module.exports = emailRouter;
